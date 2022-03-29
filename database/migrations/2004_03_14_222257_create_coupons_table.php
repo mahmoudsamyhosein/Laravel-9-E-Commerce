bcc6a59f1541fc4e0387a7e216e0bb888b093e3a
MSHM <MAHMOUDSAMYHOSEIN@GMAIL.com>
2022-03-29 10:43:54 +0200

    app error heroku

---
+++ database/migrations/2004_03_14_222257_create_coupons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->enum('type',['fixed','percent']);
+            $table->decimal('value');
+            $table->decimal('cart_value');
+            $table->date('expiry_date')->timestamps();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons', function (Blueprint $table) {
+            $table->date('expiry_date')->timestamps();
+        });
+    }
+}
