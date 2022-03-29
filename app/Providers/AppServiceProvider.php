0739204ad71f5ff01c15050d3b53a67dc5b854c0
MSHM <MAHMOUDSAMYHOSEIN@GMAIL.com>
2022-03-29 11:12:05 +0200

    app error heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if($this->app->environment() == 'production'){
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
